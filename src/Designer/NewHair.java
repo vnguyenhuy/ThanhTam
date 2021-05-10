42269749cc0b7590cd335f075782c3d0ee944a22
william <vnguyenhuy@gmail.com>
2021-05-10 17:21:42 +0700

    7th commit

--- src/Designer/NewHair.java
+++ src/Designer/NewHair.java
@@ -5,5 +5,6 @@ public class NewHair {
 	public static void main(String[] args) {
 		System.out.println("Nhung ngoi sao tren cao, la nguoi ban tam giao!!!");
 		System.out.println("Thanh tam de thuong ghe!!!!");
+		System.out.println("De thuong nhat nha!!!!");
 	}
 }
